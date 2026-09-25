275e1fbb7699f937764ed7cc2fbbbcfa92d4b303
Rickard Lund <rickard.lund@havochvatten.se>
2026-09-25 13:06:45 +0200

    Show DDM format with 4 decimals instead of 2

--- app/src/app/modules/map/components/asset-panel-show/asset-panel-show.component.ts
+++ app/src/app/modules/map/components/asset-panel-show/asset-panel-show.component.ts
@@ -1,22 +1,22 @@
-import { Component, Input } from '@angular/core';
-import { formatDate } from '@app/helpers/helpers';
-import { AssetTypes } from '@data/asset';
-import { Position } from '@data/generic.types';
+import { Component, Input } from "@angular/core";
+import { formatDate } from "@app/helpers/helpers";
+import { AssetTypes } from "@data/asset";
+import { Position } from "@data/generic.types";
 
-import { formatUnixtime } from '@app/helpers/datetime-formatter';
-import { convertDDToDDM } from '@app/helpers/wgs84-formatter';
+import { formatUnixtime } from "@app/helpers/datetime-formatter";
+import { convertDDToDDM } from "@app/helpers/wgs84-formatter";
 
-import getContryISO2 from 'country-iso-3-to-2';
+import getContryISO2 from "country-iso-3-to-2";
 
 // @ts-ignore
-import moment from 'moment-timezone';
-import { ActivityTypes } from '@data/activity';
+import moment from "moment-timezone";
+import { ActivityTypes } from "@data/activity";
 
 @Component({
-    selector: 'map-asset-panel-show',
-    templateUrl: './asset-panel-show.component.html',
-    styleUrls: ['./asset-panel-show.component.scss'],
-    standalone: false
+  selector: "map-asset-panel-show",
+  templateUrl: "./asset-panel-show.component.html",
+  styleUrls: ["./asset-panel-show.component.scss"],
+  standalone: false,
 })
 export class AssetPanelShowComponent {
   @Input() asset: AssetTypes.AssetData;
@@ -25,7 +25,11 @@ export class AssetPanelShowComponent {
 
   @Input() deselectAsset: (assetId: string) => void;
   @Input() getAssetTrack: (assetId: string, movementId: string) => void;
-  @Input() getAssetTrackTimeInterval: (assetId: string, startDate: number, endDate: number) => void;
+  @Input() getAssetTrackTimeInterval: (
+    assetId: string,
+    startDate: number,
+    endDate: number,
+  ) => void;
   @Input() getIncidentsForAssetId: (assetId: string) => void;
   @Input() getLicenceForAsset: (assetId: string) => void;
   @Input() untrackAsset: (assetId: string) => void;
@@ -53,42 +57,42 @@ export class AssetPanelShowComponent {
   // Extracting this code to separete function so we can override this code in unit-tests.
   private getTracksMillisecondCap() {
     const tracksMillisecondCap = this.tracksMinuteCap * 60 * 1000;
-    return moment().subtract(tracksMillisecondCap, 'ms').format('x');
+    return moment().subtract(tracksMillisecondCap, "ms").format("x");
   }
 
   public toggleTracks = (asset: AssetTypes.AssetData) => {
-    if(this.tracksIsVisible(asset)) {
+    if (this.tracksIsVisible(asset)) {
       this.untrackAsset(asset.asset.id);
-    } else if(this.tracksMinuteCap === null) {
+    } else if (this.tracksMinuteCap === null) {
       this.getAssetTrack(asset.asset.id, asset.currentPosition.movement.id);
     } else {
       this.getAssetTrackTimeInterval(
         asset.asset.id,
         parseInt(this.getTracksMillisecondCap()),
-        parseInt(moment().format('x'))
+        parseInt(moment().format("x")),
       );
     }
-  }
+  };
 
   public toggleForecast = (assetId: string) => {
-    if(this.forecastIsVisible(assetId)) {
+    if (this.forecastIsVisible(assetId)) {
       this.removeForecast(assetId);
     } else {
       this.addForecast(assetId);
     }
-  }
+  };
 
   public tracksIsVisible = (asset: AssetTypes.AssetData): boolean => {
-    return typeof asset.assetTracks !== 'undefined';
-  }
+    return typeof asset.assetTracks !== "undefined";
+  };
   public forecastIsVisible = (assetId: string): boolean => {
     return Object.keys(this.forecasts).indexOf(assetId) !== -1;
-  }
+  };
 
   public getCountryCode() {
     const countryCode = getContryISO2(this.asset.asset.flagStateCode);
-    if(typeof countryCode === 'undefined') {
-      return '???';
+    if (typeof countryCode === "undefined") {
+      return "???";
     }
     return countryCode.toLowerCase();
   }
@@ -102,20 +106,29 @@ export class AssetPanelShowComponent {
   }
 
   formatLocation(location: Position) {
-    const formattedPosition = convertDDToDDM(location.latitude, location.longitude, 2);
-    return formattedPosition.latitude + ' ' + formattedPosition.longitude;
+    const formattedPosition = convertDDToDDM(
+      location.latitude,
+      location.longitude,
+      4,
+    );
+    return formattedPosition.latitude + " " + formattedPosition.longitude;
   }
 
   translateOceanRegion(sourceSatelliteId: string) {
-    return AssetTypes.OceanRegionTranslation[sourceSatelliteId] || sourceSatelliteId;
+    return (
+      AssetTypes.OceanRegionTranslation[sourceSatelliteId] || sourceSatelliteId
+    );
   }
 
   getAisClass() {
-    if(typeof this.selectedAssetsLastPositions.ais.status !== 'undefined' && this.selectedAssetsLastPositions.ais.status > '' ) {
+    if (
+      typeof this.selectedAssetsLastPositions.ais.status !== "undefined" &&
+      this.selectedAssetsLastPositions.ais.status > ""
+    ) {
       const typeNr = parseInt(this.selectedAssetsLastPositions.ais.status, 10);
-      if(typeNr >= 1 && typeNr <= 3) {
+      if (typeNr >= 1 && typeNr <= 3) {
         return $localize`:@@ts-movement-ais-class-a:Class A`;
-      } else if(typeNr === 18) {
+      } else if (typeNr === 18) {
         return $localize`:@@ts-movement-ais-class-b:Class B`;
       }
     }
--- app/src/app/modules/map/components/asset-panel/asset-panel.component.ts
+++ app/src/app/modules/map/components/asset-panel/asset-panel.component.ts
@@ -1,28 +1,32 @@
-import { Component, Input } from '@angular/core';
-import { formatDate } from '@app/helpers/helpers';
-import { AssetTypes } from '@data/asset';
-import { Position } from '@data/generic.types';
+import { Component, Input } from "@angular/core";
+import { formatDate } from "@app/helpers/helpers";
+import { AssetTypes } from "@data/asset";
+import { Position } from "@data/generic.types";
 
-import getContryISO2 from 'country-iso-3-to-2';
+import getContryISO2 from "country-iso-3-to-2";
 
-import { formatUnixtime } from '@app/helpers/datetime-formatter';
-import { convertDDToDDM } from '@app/helpers/wgs84-formatter';
+import { formatUnixtime } from "@app/helpers/datetime-formatter";
+import { convertDDToDDM } from "@app/helpers/wgs84-formatter";
 
 // @ts-ignore
-import moment from 'moment-timezone';
+import moment from "moment-timezone";
 
 @Component({
-    selector: 'map-asset-panel',
-    templateUrl: './asset-panel.component.html',
-    styleUrls: ['./asset-panel.component.scss'],
-    standalone: false
+  selector: "map-asset-panel",
+  templateUrl: "./asset-panel.component.html",
+  styleUrls: ["./asset-panel.component.scss"],
+  standalone: false,
 })
 export class AssetPanelComponent {
   @Input() asset: AssetTypes.AssetData;
 
   @Input() deselectAsset: (assetId: string) => void;
   @Input() getAssetTrack: (assetId: string, movementId: string) => void;
-  @Input() getAssetTrackTimeInterval: (assetId: string, startDate: number, endDate: number) => void;
+  @Input() getAssetTrackTimeInterval: (
+    assetId: string,
+    startDate: number,
+    endDate: number,
+  ) => void;
   @Input() untrackAsset: (assetId: string) => void;
   @Input() addForecast: (assetId: string) => void;
   @Input() removeForecast: (assetId: string) => void;
@@ -44,42 +48,42 @@ export class AssetPanelComponent {
   // Extracting this code to separete function so we can override this code in unit-tests.
   private getTracksMillisecondCap() {
     const tracksMillisecondCap = this.tracksMinuteCap * 60 * 1000;
-    return moment().subtract(tracksMillisecondCap, 'ms').format('x');
+    return moment().subtract(tracksMillisecondCap, "ms").format("x");
   }
 
   public toggleTracks = (asset: AssetTypes.AssetData) => {
-    if(this.tracksIsVisible(asset)) {
+    if (this.tracksIsVisible(asset)) {
       this.untrackAsset(asset.asset.id);
-    } else if(this.tracksMinuteCap === null) {
+    } else if (this.tracksMinuteCap === null) {
       this.getAssetTrack(asset.asset.id, asset.currentPosition.movement.id);
     } else {
       this.getAssetTrackTimeInterval(
         asset.asset.id,
         parseInt(this.getTracksMillisecondCap()),
-        parseInt(moment().format('x'))
+        parseInt(moment().format("x")),
       );
     }
-  }
+  };
 
   public toggleForecast = (assetId: string) => {
-    if(this.forecastIsVisible(assetId)) {
+    if (this.forecastIsVisible(assetId)) {
       this.removeForecast(assetId);
     } else {
       this.addForecast(assetId);
     }
-  }
+  };
 
   public tracksIsVisible = (asset: AssetTypes.AssetData): boolean => {
-    return typeof asset.assetTracks !== 'undefined';
-  }
+    return typeof asset.assetTracks !== "undefined";
+  };
   public forecastIsVisible = (assetId: string): boolean => {
     return Object.keys(this.forecasts).indexOf(assetId) !== -1;
-  }
+  };
 
   public getCountryCode() {
     const countryCode = getContryISO2(this.asset.asset.flagStateCode);
-    if(typeof countryCode === 'undefined') {
-      return '???';
+    if (typeof countryCode === "undefined") {
+      return "???";
     }
     return countryCode.toLowerCase();
   }
@@ -89,16 +93,21 @@ export class AssetPanelComponent {
   }
 
   formatLocation(location: Position) {
-    const formattedPosition = convertDDToDDM(location.latitude, location.longitude, 2);
-    return formattedPosition.latitude + ' ' + formattedPosition.longitude;
+    const formattedPosition = convertDDToDDM(
+      location.latitude,
+      location.longitude,
+      4,
+    );
+    return formattedPosition.latitude + " " + formattedPosition.longitude;
   }
 
   translateOceanRegion(sourceSatelliteId: string) {
-    return AssetTypes.OceanRegionTranslation[sourceSatelliteId] || sourceSatelliteId;
+    return (
+      AssetTypes.OceanRegionTranslation[sourceSatelliteId] || sourceSatelliteId
+    );
   }
 
   public selectAssetWrapper() {
     return () => this.selectAsset(this.asset.asset.id);
   }
-
 }
